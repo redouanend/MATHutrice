aadf35744ea4daefb5a08fa2e17c326f21b8febd
Jiro-science <jiroaugoula91@gmail.com>
2026-03-13 14:08:11 +0100

    [FRONTEND] affichage des qcm en fonction de la notion

--- generator_test/app.py
+++ generator_test/app.py
@@ -4,6 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import uvicorn
 from pydantic import BaseModel
+from test_format_generator.QCM import generate_qcm_statement
 
 app = FastAPI()
 
@@ -23,8 +24,12 @@ class Data(BaseModel):
 
 # Récupération de la requête contenant le nom de la notion (envoyer par le frontend)
 @app.post("/generate_exo", include_in_schema=True)
-async def get_notion(data: Data):
-    print(data.notion)
+async def get_notion(request: Request, data: Data):
+    qcm_data = generate_qcm_statement(data.notion, niveau="légendaire")
+
+    return templates.TemplateResponse(
+        "qcm.html", {"request": request, "questions": [qcm_data]}
+    )
 
 
 uvicorn.run(app, host="localhost", port=8000)
